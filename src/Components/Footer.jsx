e9677de89005f7a97f28706db15e5687486ee381
Sharkolly <sharkollymofeoluwa@gmail.com>
2024-04-11 13:47:07 +0100

    first commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-9/12 mx-auto mt-20 mb-16 flex justify-between max-lg:w-10/12 max-lg:mt-12 max-lg:justify-center max-sm:w-[85%] max-sm:flex-col max-sm:mt-10'>
-      <div className='max-lg:hidden'>
-        <h1 className='text-2xl font-bold max-lg:hidden'>DIMATRI</h1>
+    <div className='w-9/12 mx-auto mt-20 mb-16 flex justify-between max-lg:w-10/12 max-lg:mt-12 max-xl:justify-center max-sm:w-[85%] max-sm:flex-col max-sm:mt-10'>
+      <div className='max-xl:hidden'>
+        <h1 className='text-2xl font-bold'>DIMATRI</h1>
       </div>
-      <div className='flex justify-between gap-16 text-center flex-wrap max-lg:gap-12 max-sm:text-left mt-5 max-sm:gap-8'>
+      <div className='flex justify-between gap-16 text-center flex-wrap max-lg:gap-12 max-lg:text-left max-sm:mt-5 max-sm:gap-8'>
         <div className='flex flex-col gap-4 opacity-85 max-sm:  '>
           <h1 className='text-xl font-bold'>Home</h1>
           
